cbfc7a197dc530106b7ebe09b7d273291faf3efc
Zak Estrada <estrada@rose-hulman.edu>
2019-09-26 13:39:23 -0400

    Refactored timing precision test for current UART interface

--- unit_test_scripts/tda2_timing_precision.py
+++ unit_test_scripts/tda2_timing_precision.py
@@ -6,32 +6,47 @@ for comport in list(serial.tools.list_ports.comports()):
     if "FTDI" in comport[2]:
         #Assumption: this is the only FTDI device
         port = comport[0]
+print("Using serial port", port)
 ser = serial.Serial(port, baudrate=115200, timeout=1)
 
-ch_signal = ([0] * 521) + [1, 0, 1] + ([1] * 521)
-
-for ch in range(4):
-	ser.write('set_rec_mode {}\n'.format(ch).encode('utf-8'))
-
-time.sleep(1)
-
 ch = 0
-ser.write('en_wram {}\n'.format(ch).encode('utf-8'))
+freq = 1000000 #output frequency
+
+ch_signal = ([0] * 521) + [1, 0, 1] + ([1] * 521)
+nsamples = len(ch_signal) #Number of samples
+
+#Configure the sample rate on the selected channel
+ser.write('set_freq {} {}\n'.format(ch, freq).encode('utf-8'))
+
 time.sleep(1)
 
+#Set the stop address for the channel: number of samples - 1
+ser.write('set_stop_addr {} {}\n'.format(ch,nsamples-1).encode('utf-8'))
+time.sleep(1)
+
+#Get ready to load raw values
+ch_mask = (1 << ch) #Channel mask
+cmd='load {:X} 0 {}\n'.format(ch_mask, nsamples).encode('utf-8')
+ser.write(cmd)
 for addr, val in enumerate(ch_signal):
-	print('CH {} ADDR {}, {}'.format(ch, addr, val))
-	ser.write('wram {} {} {}\n'.format(ch, addr, val).encode('utf-8'))
+    #Now write the raw bits
+    print('CH {:X} ADDR {}, {}'.format(ch, addr, val))
+    #Shift by channel number since the position of the bit determines which
+    #channel this bit is for it is
+    ser.write(bytes(val<<ch)) 
 
-	time.sleep(0.01)
+#Debugging - force clear the input buffer?
+ser.write(bytes(0)) 
+ser.write("\n".encode('utf-8'))
 
-ser.write('set_addr {} 0\n'.format(ch).encode('utf-8'))
-ser.write('dis_wram {}\n'.format(ch).encode('utf-8'))
-
-ser.write('set_freq {} 1000000\n'.format(ch).encode('utf-8'))
-
-ser.write('en_ch {}\n'.format(ch).encode('utf-8'))
+print("Number of samples: ", nsamples)
 
 input('Press enter to play signal...')
 
-ser.write('set_play_mode {}\n'.format(ch).encode('utf-8'))
+ser.write('play {}\n'.format(ch).encode('utf-8'))
+
+rsp=''
+while not rsp.endswith("OK"):
+    c = ser.read(1).decode('utf-8')
+    print(c, end='')
+    rsp += c
